66e9cb08b8fbed1d611f4afcc87df5fed1ab0f36
Adam Morehouse <patriot3g@protonmail.com>
2024-11-19 13:35:42 -0500

    Update PandasQuestion2MissingData.py

--- PandasQuestion2MissingData.py
+++ PandasQuestion2MissingData.py
@@ -12,4 +12,11 @@ df_filled = df.fillna(df.mean())
 
 print(df_filled)
 
+# This will output:
+#      A    B      C
+# 0  1.0  5.0   6.00
+# 1  2.0  4.0   8.25
+# 2  3.0  3.0   8.00
+# 3  4.0  2.0   9.00
+# 4  5.0  3.5  10.00
 
